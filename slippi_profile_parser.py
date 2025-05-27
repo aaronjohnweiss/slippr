aba0b9eb49f0b7b20df5ba361545b1bd94b81f63
Aaron Weiss <aaronjohnweiss@gmail.com>
2025-05-27 07:36:03 -0400

    feat: adjust slippi api url and object structure

--- slippi_profile_parser.py
+++ slippi_profile_parser.py
@@ -6,16 +6,11 @@ options = Options()
 options.add_argument("-headless")
 
 base_url = 'https://slippi.gg/user/'
-api_url = 'https://gql-gateway-2-dot-slippi.uc.r.appspot.com/graphql'
+api_url = 'https://internal.slippi.gg/graphql'
 
-post_object = {
-  'operationName': 'AccountManagementPageQuery',
-  'variables': {
-    'cc': 'SYS#0',
-    'uid': 'SYS#0'
-  },
-  'query': 'fragment profileFieldsV2 on NetplayProfileV2 {\n  id\n  ratingOrdinal\n  ratingUpdateCount\n  wins\n  losses\n  dailyGlobalPlacement\n  dailyRegionalPlacement\n  continent\n  characters {\n    character\n    gameCount\n    __typename\n  }\n  __typename\n}\n\nfragment userProfilePage on User {\n  fbUid\n  displayName\n  connectCode {\n    code\n    __typename\n  }\n  status\n  activeSubscription {\n    level\n    hasGiftSub\n    __typename\n  }\n  rankedNetplayProfile {\n    ...profileFieldsV2\n    __typename\n  }\n  rankedNetplayProfileHistory {\n    ...profileFieldsV2\n    season {\n      id\n      startedAt\n      endedAt\n      name\n      status\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nquery AccountManagementPageQuery($cc: String!, $uid: String!) {\n  getUser(fbUid: $uid) {\n    ...userProfilePage\n    __typename\n  }\n  getConnectCode(code: $cc) {\n    user {\n      ...userProfilePage\n      __typename\n    }\n    __typename\n  }\n}\n'
-}
+post_object = {"operationName": "UserProfilePageQuery", "variables": {"cc": "ASIG#701", "uid": "ASIG#701"},
+                "query": "fragment profileFields on NetplayProfile {\n  id\n  ratingOrdinal\n  ratingUpdateCount\n  wins\n  losses\n  dailyGlobalPlacement\n  dailyRegionalPlacement\n  continent\n  characters {\n    character\n    gameCount\n    __typename\n  }\n  __typename\n}\n\nfragment userProfilePage on User {\n  fbUid\n  displayName\n  connectCode {\n    code\n    __typename\n  }\n  status\n  activeSubscription {\n    level\n    hasGiftSub\n    __typename\n  }\n  rankedNetplayProfile {\n    ...profileFields\n    __typename\n  }\n  rankedNetplayProfileHistory {\n    ...profileFields\n    season {\n      id\n      startedAt\n      endedAt\n      name\n      status\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nquery UserProfilePageQuery($cc: String, $uid: String) {\n  getUser(fbUid: $uid, connectCode: $cc) {\n    ...userProfilePage\n    __typename\n  }\n}\n"}
+
 post_headers = {
     'content-type': 'application/json',
     'apollographql-client-name': 'slippi-web',
@@ -67,10 +62,9 @@ def get_user_from_tag(tag):
     request_json = post_object.copy()
     request_json['variables']['cc'] = user.tag.upper()
     request_json['variables']['uid'] = user.tag.upper()
-
     x = requests.post(api_url, json=request_json, headers=post_headers)
-    print(x.json())
-    user_data = x.json()['data']['getConnectCode']['user']
+    # print(x.json())
+    user_data = x.json()['data']['getUser']
     user.name = user_data['displayName']
     user.elo = user_data['rankedNetplayProfile']['ratingOrdinal']
     user.regional_placement = user_data['rankedNetplayProfile']['dailyRegionalPlacement']
